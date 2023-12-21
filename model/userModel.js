bfbae10a8a44ff4f7e91d12cec89714c18dd8dfe
Sathishramesh1 <sathishrameshkec@gmail.com>
2023-12-21 21:58:45 +0530

    basic-structure

---
+++ model/userModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema=mongoose.Schema({
+    name:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    email:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    password:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    pic:{
+        type:String,
+        default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false,
+      },
+},
+{
+    timestamps: true
+}
+);
+const User=mongoose.model("User",userSchema);
+
+export {User}
