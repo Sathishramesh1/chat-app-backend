538b3ea6c81fbac5235233d5ceb272a27db91e6a
Sathishramesh1 <sathishrameshkec@gmail.com>
2024-01-22 16:39:12 +0530

    chat-controller

--- controller/chatController.js
+++ controller/chatController.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { Chat } from "../model/chatModel";
+import { User } from "../model/userModel";
 
 
 const accessChat=async(req,res)=>{
@@ -8,11 +9,39 @@ const accessChat=async(req,res)=>{
 
     if(!userId){
         console.log("UserId param not sent with req");
-    return res.status(400).send("unable to find the userId");
+    return res.status(400).send("unable to find the userId");}
+
+var isChat=await Chat.find({
+    isGroupChat:false,
+    $and:[
+        {users:{$eleMatch:{$eq:req.user._id}}},
+        {users:{$eleMatch:{$eq:userId}}},
+    ]    
+}).populate("users","-password").populate("latestMessage");
+
+isChat=await User.populate(isChat,{
+    path: "latestMessage.sender",
+    select:"name pic email",
+});
+
+if(isChat.length>0){
+    res.status(200).send(isChat[0]);
+} else{
+    var chatData={
+        chatName:"sender",
+        isGroupChat:false,
+        users:[req.user._id,userId],
+};
 }
 
-const isChat=await Chat.find({
-    
-})
+try {
+    const createChat=await Chat.create(chatData);
+    const FullChat= await Chat.findOne({_id:createChat._id}).populate("users","-password");
 
+    res.status(200).send(FullChat);
+    
+} catch (error) {
+    res.status(400).send(error);
+
+}
 }
